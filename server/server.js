a7de46face24576bb5a883a9b94196ccc0c9dbe5
eumcloud <livelch012@naver.com>
2021-12-30 23:59:27 +0900

    imd server connect

--- server/server.js
+++ server/server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const boards = require("./src/routes/board/Board");
-const products = require("./src/routes/product/Product");
-const mains = require("./src/routes/main/Main");
-const mypages = require("./src/routes/mypage/Mypage");
+const boards = require("./router/board/Board");
+const products = require("./router/product/Product");
+const mains = require("./router/main/Main");
+const mypages = require("./router/mypage/Mypage");
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.use("/board", boards);
@@ -22,8 +22,8 @@ app.get("*", (req, res)=>{
 });
 
 
-const host = "127.0.0.1";
-const port = 3000;
+const host = process.env.NODEHOST || "127.0.0.1";
+const port = process.env.NODEPORT || 5000;
 app.listen(port, ()=>{
     console.log(`ReactProject is running  ---http://${host}:${port}`);
 });
