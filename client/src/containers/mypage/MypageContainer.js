37e08a1d1a48966ec5fdc46a7d1f9eeffabba748
eumcloud <livelch012@naver.com>
2021-12-31 18:58:34 +0900

    router 0.02

--- client/src/containers/mypage/MypageContainer.js
+++ client/src/containers/mypage/MypageContainer.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Footer from '../../components/main/Footer';
 import NavigationBar from '../../components/main/NavigationBar'
-import Headline from "../../components/mypage/Headline";
-import MypageList from "../../components/mypage/MypageList";
-import Template from "../../components/mypage/MypageTemplate";
+import Headline from "../../components/mypage/list/Headline";
+import MypageList from "../../components/mypage/list/MypageList";
+import Template from "../../components/mypage/list/Template";
 
 export default function MypageContainer() {
     return (
         <div>
-            
+            <NavigationBar />
             <Template>
                 <Headline />
                 <MypageList />    
