ddaabfb31dd859844ce9baa9bb01c83d2211663c
eumcloud <livelch012@naver.com>
2022-01-11 14:40:34 +0900

    -

--- client/src/index.js
+++ client/src/index.js
@@ -6,15 +6,15 @@ import { Provider } from "react-redux";
 import rootReducer from "./context/"
 import { composeWithDevTools} from "redux-devtools-extension";
 
-const store = createStore(rootReducer, composeWithDevTools()); //make store 
-console.log(store.getState()); //store state pring to console
+// const store = createStore(rootReducer, composeWithDevTools()); //make store 
+// console.log(store.getState()); //store state pring to console
 ReactDOM.render(
-<Provider store={store}>
+
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  </Provider>,
-  document.getElementById('root')
+  
+  ,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
