6875bff29b0a1318e61c0ccdab20ef04c2e641a7
eumcloud <livelch012@naver.com>
2022-01-11 14:07:21 +0900

    -- packcage.json

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore} from "redux"; 
+import { Provider } from "react-redux";
+import rootReducer from "./context/"
+import { composeWithDevTools} from "redux-devtools-extension";
 
+const store = createStore(rootReducer, composeWithDevTools()); //make store 
+console.log(store.getState()); //store state pring to console
 ReactDOM.render(
+<Provider store={store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
