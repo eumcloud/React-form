c78318a8e8a59cea51f33ec0d5c43194be5a23fa
Sungjin <93691850+sungjinWi@users.noreply.github.com>
2022-01-11 11:01:03 +0900

    comment&hit

--- client/src/components/board/CommentList.js
+++ client/src/components/board/CommentList.js
@@ -12,51 +12,78 @@ import { TextareaAutosize } from "@mui/material";
 
 
 
-export default function CommentList() {
-  const [inputData, setInputData] = useState([{
-    cidx: '',
-    cuserid: '',
-  }])
-
+export default function CommentList({bnum}) {
+  const [commentData, setCommentData] = useState([])
   const callApi = async() => {
     const response = await axios.get("http://localhost:3001/api/comments")
-    console.log(inputData)
-    console.log(response.data)
-    setInputData([...inputData,...response.data])
+    setCommentData(response.data.filter(comment => comment.board_idx === bnum))
   }
   
   useEffect(() => {
     callApi();
-  }, []);
+  }, [commentData]);
 
-  console.log(inputData)
-  
+
+  const [cinput,setCinput] = useState({
+    cuserid:'로그인id',
+    ccontent:'',
+    board_idx: bnum
+  })
+
+
+
+  const onChange = (e) => {
+    const {value, name} = e.target;
+
+    setCinput({
+      ...cinput,
+      [name]: value
+    });
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:3001/api/comments",cinput)
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+        console.log(error.response.data)
+        alert(error.response.data.error)
+    })
+    setCinput({
+      cuserid:'로그인id',
+      ccontent:'',
+      board_idx:bnum
+    });
+
+  }
 
   return (
     <>
     <form onSubmit={onSubmit}>
-      <TextareaAutosize name="btitle" value={inputData.btitle} onChange={onChange}/>
+      <input type="hidden" name="cuserid" value={cinput.cuserid} />
+      <input name="ccontent" value={cinput.ccontent} onChange={onChange}/>
+      <button type="submit">댓글 작성</button>
     </form>
     <TableContainer >
       <Table sx={{ maxWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>index</TableCell>
-            <TableCell align="right">userid</TableCell>
-            <TableCell align="right">content</TableCell>
+            <TableCell>userid</TableCell>
+            <TableCell >content</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {inputData.map((row) => (
+          {commentData.map((row) => (
             <TableRow
-              key={row.bidx}
+              key={row.cidx}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.buserid}
+              {row.cuserid}
               </TableCell>
-              <TableCell align="right">{row.btitle}</TableCell>
-              <TableCell align="right">{row.bhit}</TableCell>
+              <TableCell >{row.ccontent}</TableCell>
             </TableRow>
           ))}
         </TableBody>
