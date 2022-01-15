4ea1b6c85925933e98a65e19b45114b3786e0628
junghyunB <bjh940126@gmail.com>
2022-01-15 19:17:45 +0900

    final product

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,6 @@ function App() {
                     <Router />
                   </main> 
               </Container>
-      
             <Footer />
             {/* <BoardRoute /> */}
           </div>
--- client/src/containers/product/ProductDetail.js
+++ client/src/containers/product/ProductDetail.js
@@ -10,6 +10,7 @@ import Template from "./Template"
 import {useNavigate} from "react-router-dom"
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import Payment from "./Payment";
 
 
 const Fontbutton = styled.div`
@@ -61,6 +62,11 @@ function ProductDetail() {
         e.preventDefault();
         navigate(-1);
       } 
+
+      // function buyPage(e) {
+      //   e.preventDefault();
+      //   navigate("/Payment")
+      // }
       
       const [number, setNumber] = useState(1);
 
@@ -74,6 +80,48 @@ function ProductDetail() {
 
       const Totalprice = number * inputData.realprice
 
+      useEffect(() => 
+      { const jquery = document.createElement("script");
+      jquery.src = "https://code.jquery.com/jquery-1.12.4.min.js"; 
+      const iamport = document.createElement("script"); 
+      iamport.src = "https://cdn.iamport.kr/js/iamport.payment-1.1.7.js";
+      document.head.appendChild(jquery);
+      document.head.appendChild(iamport);
+      return () => {
+          document.head.removeChild(jquery);
+          document.head.removeChild(iamport);
+      }
+  },[]); 
+
+    const onClickPayment = () => { 
+      const { IMP } = window;
+      IMP.init("imp31715307");
+      //결제 데이터 정의
+      const data = 
+      { pg: "html_inicis",
+        pay_method: "card",
+        merchant_uid:`mid_${new Date().getTime()}`,
+        name: "변정현",
+        amount: `${Totalprice}`,
+        custom_data: {
+            name: "부가정보",
+            desc: "세부 부가정보"
+        },
+        buyer_name: "변정현",
+        buyer_tel: "01065512902",
+        buyer_email: "wjdgus2903@naver.com",
+        buyer_addr: "강동구 천호",
+        buyer_postalcode: "05258"
+  };
+
+  IMP.request_pay(data,callback);
+  }
+  const callback = (response) => {
+      const { success, error_msg, imp_uid, merchant_uid, pay_method, paid_amount, status} = response;
+      if (success) { alert('결제 성공');}
+      else { alert(`결제 실패 : ${error_msg}`);}
+  }
+
       return (
         <>     
         <Template> 
@@ -84,20 +132,20 @@ function ProductDetail() {
               <TableBody>
                 <TableRow>
                   <TableCell></TableCell>
-                  <TableCell>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <TableCell>
                     <img src={inputData.image} style={imagestyle} /></TableCell>
                 </TableRow>
                 <TableRow>       
                   <TableCell>번호</TableCell>
-                  <TableCell>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <TableCell>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                     {inputData.id}</TableCell>
                 </TableRow>
                   <TableCell>가격</TableCell>
-                  <TableCell>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <TableCell>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                     {inputData.price}</TableCell>
                 <TableRow> 
                   <TableCell>상품명</TableCell>
-                  <TableCell>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <TableCell>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                     {inputData.product}</TableCell>
                 </TableRow>
                 <TableRow>
@@ -106,12 +154,12 @@ function ProductDetail() {
                 </TableRow>
                 <TableRow>
                   <TableCell>수량</TableCell>
-                  <TableCell>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <TableCell>
                     <Button onClick={onDecrease} variant="text">-</Button>{number}<Button onClick={onIncrease} variant="text">+</Button></TableCell>
                 </TableRow>
                 <TableRow>
                 <TableCell>결제 예정금액</TableCell>
-                <TableCell>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <TableCell>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                   KRW {Totalprice}
                   </TableCell>
                 </TableRow>
@@ -120,7 +168,8 @@ function ProductDetail() {
             <br />
             <div className="but">
               <Stack>
-              <Button variant="text">구매하기</Button><Button variant="text" onClick={backPage}>목록으로</Button>
+              <Button onClick={onClickPayment}variant="text">구매하기</Button>
+              <Button variant="text" onClick={backPage}>목록으로</Button>
               </Stack>
             </div>
             </Fontbutton>
